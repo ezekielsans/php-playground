66aab58d7a06fd3cbf15dee3c61e00bd339680c4
Ezekiel Santos <ezekielsantos.dev@gmail.com>
2024-08-21 08:21:33 +0800

    to fix docker configs

---
+++ database/mystore_db.sql
@@ -0,0 +1,206 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 21, 2024 at 02:09 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mystore`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE `members` (
+  `ID` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `mobile_no` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `access` varchar(255) NOT NULL,
+  `addded_by` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `members`
+--
+
+INSERT INTO `members` (`ID`, `first_name`, `last_name`, `email`, `password`, `mobile_no`, `address`, `access`, `addded_by`) VALUES
+(15, 'admin', 'admin', 'admin@email.test', '0192023a7bbd73250516f069df18b500', '0912345678', '', 'administrator', '0000-00-00 00:00:00'),
+(17, 'Tester', 'One', 'tester@email.test', 'cc03e747a6afbbcbf8be7668acfebee5', '09999332313', '', 'administrator', '0000-00-00 00:00:00'),
+(18, 'Ezekiel Jeremiah', 'Santos', 'ej@email.test', '482c811da5d5b4bc6d497ffa98491e38', '09560414165', '', 'user', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `ID` int(11) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `product_type` varchar(255) NOT NULL,
+  `min_stocks` int(11) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `added_by` varchar(255) NOT NULL,
+  `date_added` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`ID`, `product_name`, `product_type`, `min_stocks`, `description`, `added_by`, `date_added`) VALUES
+(1, 'Camera', 'Tools', 20, '', 'admin admin', '2024-08-16 00:02:12'),
+(2, 'Printer', 'Tools', 7, '', 'admin admin', '2024-08-16 06:13:12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_items`
+--
+
+CREATE TABLE `product_items` (
+  `ID` int(11) NOT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `qty` int(11) NOT NULL,
+  `price` varchar(255) NOT NULL,
+  `vendor` varchar(255) NOT NULL,
+  `batch_number` varchar(255) NOT NULL,
+  `addded_at` date NOT NULL DEFAULT current_timestamp(),
+  `added_by` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product_items`
+--
+
+INSERT INTO `product_items` (`ID`, `product_id`, `qty`, `price`, `vendor`, `batch_number`, `addded_at`, `added_by`) VALUES
+(1, 2, 20, '234.55', 'EPSON', '13ASDAQWE', '2024-08-17', 'admin admin'),
+(2, 2, 50, '1211.12', 'BROTHER', '1311QWEASD', '2024-08-17', 'admin admin'),
+(3, 1, 25, '88.23', 'SONY', 'QWEQ123332', '2024-08-17', 'admin admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+
+CREATE TABLE `sales` (
+  `ID` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `stocks_id` int(11) NOT NULL,
+  `brand_name` varchar(255) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `price` varchar(255) NOT NULL,
+  `customer_name` varchar(255) NOT NULL,
+  `date_purchased` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sales`
+--
+
+INSERT INTO `sales` (`ID`, `product_id`, `stocks_id`, `brand_name`, `qty`, `price`, `customer_name`, `date_purchased`) VALUES
+(1, 2, 2, 'BROTHER', 5, '6055.6', 'admin admin', '2024-08-17 13:56:17'),
+(2, 2, 1, 'EPSON', 3, '703.65', 'admin admin', '2024-08-17 13:56:17'),
+(8, 1, 3, 'SONY', 5, '441.15', 'admin admin', '2024-08-18 15:20:05'),
+(9, 2, 1, 'EPSON', 7, '1641.85', 'admin admin', '2024-08-18 15:34:04');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `members`
+--
+ALTER TABLE `members`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `product_items`
+--
+ALTER TABLE `product_items`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `product_items_ibfk_1` (`product_id`);
+
+--
+-- Indexes for table `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `sales_fkey_1` (`stocks_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `members`
+--
+ALTER TABLE `members`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `product_items`
+--
+ALTER TABLE `product_items`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `product_items`
+--
+ALTER TABLE `product_items`
+  ADD CONSTRAINT `product_items_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `sales`
+--
+ALTER TABLE `sales`
+  ADD CONSTRAINT `sales_fkey_1` FOREIGN KEY (`stocks_id`) REFERENCES `product_items` (`ID`) ON DELETE NO ACTION ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
